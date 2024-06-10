e510d89eb30d8d24479eba2817698209c3dfb5ac
Philip Schuh <philip.schuh@customaquariums.com>
2024-06-10 10:21:44 -0500

    Because I added some stuff at work

--- shell-faqs.sh
+++ shell-faqs.sh
@@ -7,7 +7,6 @@
 
 echo test-bed
 
-
 # $$ is called a special parameter. These parameters may only be referenced. Assignment to them is verboten.
 # See this link for other special parameters. https://www.gnu.org/software/bash/manual/bash.html#Special-Parameters
 # $$ Expands to the process ID (PID) of the shell. In a subshell, it expands to the process ID of the invoking shell, not the subshell.
@@ -21,5 +20,12 @@ timestamp=$(date +%Y-%m-%d-%H%M)
 echo $timestamp
 
 # cron jobs don't appear to be able to access envronment variables. If using environment variables, it seems that the env variables need to be explicitly
-# called in the script.
+# called in the script. Check out this thread - https://stackoverflow.com/questions/2229825/where-can-i-set-environment-variables-that-crontab-will-use
 # when setting up the cron command, I may need to use the absolute path of the interpreter
+
+# associative arrays in shell
+declare -A databases=(
+  ["db1"]="client_db1"
+  ["db2"]="client_db2"
+  ["db3"]="client_db3"
+)
