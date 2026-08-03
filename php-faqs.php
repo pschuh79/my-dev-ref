91da9a6c3f990a0ca15c03c047976fc5894b0d5f
Philip Schuh <philip.schuh@customaquariums.com>
2026-08-03 12:50:34 -0500

    Add header comment field meanings

--- php-faqs.php
+++ php-faqs.php
@@ -10,3 +10,20 @@
  */
 
 echo __DIR__;
+
+
+/**
+ * *** Wordpress Plugin Header Comment options ***
+ * Plugin Name: Name of the Plugin
+ * Plugin URI: https://github.com/your-url.com
+ * Description: A short description of the purpose of the plugin and what it does.
+ * Version: Version number
+ * Author: The Plugin Author
+ * Author URI: https://github.com/your-url.com
+ * License: The license of the plugin.
+ * Requires PHP: The minimum required PHP version
+ * Tested up to: The most recent version of Wordpress the the plugin has been tested on
+ * Requires at least: The lowest WordPress version that the plugin will work on.
+ * WC requires at least: 6.1
+ * WC tested up to: 6.1
+ */
